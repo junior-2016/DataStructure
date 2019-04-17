ba7f375934fb85b79f7a5b6ee6581a01ee1c7a01
junior <457030053@qq.com>
2019-04-17 20:34:41 +0800

    add TODO:KD tree

---
+++ src/KdTree.h
@@ -0,0 +1,13 @@
+//
+// Created by junior on 19-4-17.
+//
+
+#ifndef DATASTRUCTURE_KDTREE_H
+#define DATASTRUCTURE_KDTREE_H
+/**
+ * TODO: 实现 KNN 的 KD-Tree 树结构
+ *   1. 欧式空间KD-Tree分割.
+ *   2. 超球体 KD-Tree (ball tree) => can use kernel KNN
+ *   reference : https://zhuanlan.zhihu.com/p/62450795
+ */
+#endif //DATASTRUCTURE_KDTREE_H
